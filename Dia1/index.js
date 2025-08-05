156c55adc6dd2da7baa8a18301441e236aced9f2
Danny200523 <danicrack0709@gmail.com>
2025-08-05 09:05:59 -0500

    feat :sparkles: first commit

---
+++ Dia1/index.js
@@ -0,0 +1,77 @@
+function MenuPrincipal(){
+    console.log(  "==============================\n"+
+            "=  Bienvenido a Campuslands  =\n"+ 
+            "==============================\n"+ 
+            "=       ¿Quien eres?         =\n"+ 
+            "==============================\n"+
+            "=     (1). Camper            =\n"+
+            "=     (2). Trainer           =\n"+
+            "=     (3). Cordinadora       =\n"+
+            "=     (4). Salir             =\n"+
+            "==============================\n");
+}
+///MENU PARA LOS CAMPERS///
+function MenuCamper(){
+    console.log(  "==============================\n"+
+            "=     ¿Estás Inscrito?       =\n"+
+            "==============================\n"+
+            "=          (1). SI           =\n"+
+            "=          (2). NO           =\n"+
+            "==============================");
+}
+///MENU POR SI UN CAMPER NO ESTA INSCRITO///
+function MenuInscripcionCamper(){
+    console.log(  "==============================\n"+
+            "= Lo siento, debes estar inscrito para ingresar  =\n"+
+            "==============================");
+}
+///MENU POR SI UN CAMPER ESTA INSCRITO///
+function SiInscrito(){
+    console.log(  "============================\n"+
+            "=    Bienvenido, Camper     =\n"+
+            "============================");
+}
+///MENU DE TRAINER///
+function MenuTrainer(){
+    console.log(  "==========================\n"+
+            "=   Bienvenido, Trainer   =\n"+
+            "==========================\n"+
+            "==========================\n"+
+            "=   ¿Qué deseas hacer?    =\n"+
+            "==========================\n"+
+            "=   1. Ver notas          =\n"+
+            "=   2. Editar notas       =\n"+
+            "=   3. Ver estudiantes    =\n"+
+            "==========================");
+}
+////MENU CAMPER////
+function menucamp(){
+    console.log("================================\n"+
+          "=       1.  VER NOTAS          =\n"+
+          "================================")
+}
+///MENU DE CORDINADORA///
+function Menucordinadora(){
+    console.log(  "=========================================\n"+
+            "=          ¿Qué desea hacer?            =\n"+
+            "=========================================\n"+
+            "=   (1). Agregar estudiante             =\n"+
+            "=   (2). Editar estudiante              =\n"+
+            "=   (3). Ver estudiantes                =\n"+
+            "=   (4). Eliminar estudiante            =\n"+
+            "=   (5). Agregar nueva ruta de estudio  =\n"+
+            "=   (6). Ver menú de reportes           =\n"+
+            "=   (7). Volver al menú principal       =\n"+
+            "=========================================");
+}
+///MENU DE REPORTES///
+function MenuReportes(){
+    console.log(  "==============================================\n"+
+            "=             ¿Qué desea hacer?              =\n"+
+            "==============================================\n"+
+            "= (1). Ver campers en estado inscrito        =\n"+
+            "=(2). Ver campers que aprobaron examen inicial=\n"+
+            "= (3). Ver trainers                          =\n"+
+            "= (4). Mostrar campers con riesgo alto       =\n"+
+            "==============================================");
+}
